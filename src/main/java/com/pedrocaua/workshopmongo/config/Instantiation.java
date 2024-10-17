8c232acabae5403e28d52bcfb5628d774548c842
Pedro Caua <pedro.cauasalgado@gmail.com>
2024-10-17 14:41:33 -0300

    Referenciando os posts do usuário

--- src/main/java/com/pedrocaua/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pedrocaua/workshopmongo/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
+		maria.getPosts().addAll(Arrays.asList(p1, p2));
+		userRepository.save(maria);
+		
 		
 		
 		
